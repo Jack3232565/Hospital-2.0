50c4d05ac2d960bff4cfd1dfde3d74a31761e54a
Your Name <210237@utxicotepec.edu.com>
2024-06-13 20:55:09 -0600

    Agregando el Footer al Dashboart

---
+++ Frond-End/Sistema-hospital/src/components/Pie_pagina.vue
@@ -0,0 +1,34 @@
+<template>
+    <footer class="bg-slate-800 rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400 font-bold">© 2024 <a href="https://flowbite.com/" class="hover:underline">Privilege Care™</a>. Hospital de Especialidades.
+        </span>
+        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6 font-bold">Nosotros</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6 font-bold">Politica de Privacidad</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline me-4 md:me-6 font-bold">Permisos</a>
+            </li>
+            <li>
+                <a href="#" class="hover:underline font-bold">Contacto</a>
+            </li>
+        </ul>
+        </div>
+    </footer>
+    
+  </template>
+  
+  <script>
+  export default {
+    name: 'Pie_pagina',
+  };
+  </script>
+  
+  <style scoped>
+  /* Agrega estilos adicionales si es necesario */
+  </style>
+  
